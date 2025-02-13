3ddbb925c6e477e70379a1f417de1fcda2d655e1
misspatriana <jnespaduga0@gmail.com>
2025-02-14 07:57:53 +0800

    java file
    Added HelloWorld class

--- IT13A_PATRIANA/src/Prelim/Act1_Arithmetic_Operators.java
+++ IT13A_PATRIANA/src/Prelim/Act1_Arithmetic_Operators.java
@@ -1,13 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Prelim;
 
-/**
- *
- * @author Administrator
- */
+
 public class Act1_Arithmetic_Operators {
-    
+     public static void main(String[] org){
+             
+        int numb1 = 10;
+        int numb2 = 6;
+        int numb3 = 4;
+        int numb4 = 3; 
+        
+        
+        
+        int patriana1 = numb1 * numb3 + numb2;
+        int miss1 = numb1 - numb3;
+        int patriana2 = miss1 % numb2;
+        int miss2 = numb1 + numb3 + numb2;
+        int patriana3 = miss2 / numb4;
+        int miss3 = numb3 * numb3;
+        int patriana4 = numb1 * numb2 - miss3;
+        
+          System.out.println("10 * 4 + 6 ="+ patriana1);
+         System.out.println("(10-4) % 6 = "+ patriana2);
+            System.out.println("(10 + 4 + 6)/3 = "+ patriana3);
+               System.out.println("10 * 6 -(4 * 4 ) = "+ patriana4);
+               
+     }
 }
